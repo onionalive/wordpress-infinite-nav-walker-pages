b5b85ae6be41634f5e74bbd04977f76fdc8eac5a
Stephen <stephenritchie0@gmail.com>
2018-03-03 15:23:48 -0800

    Create page.php

---
+++ page.php
@@ -0,0 +1,13 @@
+<?php
+
+// Displays our custom menu on the page
+wp_nav_menu( array(
+	'container'  => false,
+	'items_wrap' => '%3$s',
+	// Uses our new walker to create the menu
+	'walker' => new xyz_Mobile_Menu,
+	// chooses which menu to use
+	'theme_location' => 'menu-location'
+));
+
+?>
