c04788642531a844ab01d9661ac7abefd472d0d3
Stephen <stephenritchie0@gmail.com>
2018-03-03 15:42:19 -0800

    Update page.php

--- page.php
+++ page.php
@@ -5,7 +5,7 @@ wp_nav_menu( array(
 	'container'  => false,
 	'items_wrap' => '%3$s',
 	// Uses our new walker to create the menu
-	'walker' => new xyz_Mobile_Menu,
+	'walker' => new xyz_Custom_Menu,
 	// chooses which menu to use
 	'theme_location' => 'menu-location'
 ));
